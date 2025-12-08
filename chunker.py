081a5ed787b91b3fd50c155c6ad1a864a5400f09
teemvat <teemu.vataja@metropolia.fi>
2025-12-08 12:01:09 +0200

    Improved chunking for longer lectures

--- chunker.py
+++ chunker.py
@@ -1,3 +1,9 @@
+import uuid
+import tempfile
+import os
+from pydub import AudioSegment
+
+
 # A function used to chop long text into smaller pieces for processing.
 def chunk_text(text: str, max_words: int = 300):
 
@@ -7,5 +13,40 @@ def chunk_text(text: str, max_words: int = 300):
     for i in range(0, len(words), max_words):
         chunk = " ".join(words[i:i + max_words])
         chunks.append(chunk)
+
+    return chunks
+
+
+# A function used to chop long audio files into segments for whisper transcription.
+def chunk_audio(
+    audio_path: str,
+    chunk_length_ms: int = 60_000,    # 60 seconds
+    overlap_ms: int = 3_000           # 3 seconds overlap
+):
+
+    audio = AudioSegment.from_file(audio_path)
+    duration = len(audio)
+
+    chunks = []
+    start = 0
+
+    while start < duration:
+        end = min(start + chunk_length_ms, duration)
+
+        chunk = audio[start:end]
+
+        # Export to temporary files
+        tmp_path = os.path.join(tempfile.gettempdir(), f"{uuid.uuid4()}.wav")
+        chunk.export(tmp_path, format="wav")
+        chunks.append(tmp_path)
+
+        # Move start forward with overlap
+        start = end - overlap_ms
+
+        if start < 0:
+            start = 0
+
+        if end == duration:
+            break
 
     return chunks
--- whisper_transcription.py
+++ whisper_transcription.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from chunker import chunk_audio
 
 # Transcription uses OpenAI Whisper model via the api, so api key is required.
 load_dotenv()
@@ -9,10 +10,28 @@ client = OpenAI(api_key=api_key)
 
 
 # Transcribes audio file using Whisper model.
-def transcribe_audio(audio_path: str) -> str:
+def process_audio(audio_path: str) -> str:
     with open(audio_path, "rb") as f:
         transcript = client.audio.transcriptions.create(
             model="whisper-1",
             file=f
         )
     return transcript.text
+
+
+# Function for longer audio files:
+def transcribe_audio(audio_path: str) -> str:
+    chunk_paths = chunk_audio(audio_path)
+
+    full_text = []
+    for path in chunk_paths:
+        text = process_audio(path)
+        full_text.append(text)
+
+    for p in chunk_paths:
+        try:
+            os.remove(p)
+        except:
+            pass
+
+    return " ".join(full_text)
